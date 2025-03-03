71a8abc83a680c936a6b9c76af085166ba49d65f
Ryuz <ryuzer@proton.me>
2025-03-03 11:55:59 +0100

    fix(bun): use global sql

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -1,8 +1,7 @@
-import { SQL } from "bun";
 import { drizzle } from "drizzle-orm/bun-sql";
 import { env } from "../utils/env.utils";
 import { schema } from "./schema";
 
-const client = new SQL(env.DATABASE_URL);
+const client = new Bun.SQL(env.DATABASE_URL);
 
 export const db = drizzle({ client, schema });
