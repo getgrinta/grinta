679b0e70c1802307f31af9bb67a5744596d4e8c2
Aleksander Lorenc <thaxsillion@gmail.com>
2025-03-17 08:08:19 +0100

    feat(widgets): icons

--- apps/app/src/lib/components/widgets.svelte
+++ apps/app/src/lib/components/widgets.svelte
@@ -8,6 +8,7 @@
     } from "$lib/utils.svelte";
     import clsx from "clsx";
     import WidgetsContextMenu from "./widgets-context-menu.svelte";
+    import { appMetadataStore } from "$lib/store/app-metadata.svelte";
 
     let widgetsContextMenu = $state<WidgetsContextMenu>();
     const widgets = $derived(widgetsStore.data.widgets ?? []);
@@ -21,6 +22,7 @@
     <div class="absolute left-0 right-0 top-20 carousel gap-2 z-10">
         {#each widgets as widget, i}
             {@const IconComponent = getIcon(widget.data)}
+            {@const icon = appMetadataStore.getIcon(widget.data)}
             <div
                 class="carousel-item select-none"
                 data-widget-index={i}
@@ -38,7 +40,18 @@
                     )}
                     onclick={() => commandsStore.handleCommand(widget.data)}
                 >
-                    <IconComponent size={20} />
+                    {#if icon}
+                        <img
+                            src={icon}
+                            alt={""}
+                            width="32"
+                            height="32"
+                            class="w-6 h-6 object-contain"
+                        />
+                    {:else}
+                        <IconComponent size={20} />
+                    {/if}
+
                     <span
                         >{widget.data.localizedLabel ?? widget.data.label}</span
                     >
