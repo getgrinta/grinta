1557be92b06c700a975d88bc730939defe0df226
Ryuz <ryuzer@proton.me>
2025-04-15 22:58:03 +0200

    chore(website): replace openpanel with umami (#43)

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -7,63 +7,64 @@ import starlightBlog from "starlight-blog";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://getgrinta.com",
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	integrations: [
-		starlight({
-			title: "Grinta",
-			logo: {
-				src: "./public/logo.svg",
-				replacesTitle: true,
-			},
-			social: {
-				github: "https://github.com/getgrinta/grinta",
-				"x.com": "https://x.com/getgrinta",
-			},
-			sidebar: [
-				{
-					label: "Guides",
-					items: [
-						{ label: "Getting Started", slug: "guides" },
-						{ label: "Replace system Spotlight", slug: "guides/replace-spotlight" },
-						{ label: "Core Concepts", slug: "guides/core-concepts" },
-					],
-				},
-				{
-					label: "Core Features",
-					items: [
-						{ label: "Web and AI Search", slug: "features/search" },
-						{ label: "Notes", slug: "features/notes" },
-						{ label: "File System Search", slug: "features/file-system" },
-						{ label: "Calculator", slug: "features/calculator" },
-						{ label: "Natural Language Processing", slug: "features/nlp" },
-						{ label: "Shortcuts", slug: "features/shortcuts" },
-						{ label: "Clipboard History", slug: "features/clipboard" },
-						{ label: "Widgets", slug: "features/widgets" },
-					]
-				},
-				{
-					label: "Grinta Pro",
-					items: [
-						{ label: "Notes AI", slug: "pro/notes" },
-					]
-				},
-				{
-					label: "Recipes",
-					items: [
-						{ label: "Notes in Obsidian", slug: "recipes/obsidian" },
-						{ label: "Running Shell Commands", slug: "recipes/shell-commands" },
-						{ label: "Speech to Text", slug: "recipes/speech-to-text" },
-					]
-				}
-			],
-			customCss: ["./src/tailwind.css"],
-			head: [
-				{
-					tag: "script",
-					content: `
+  site: "https://getgrinta.com",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [
+    starlight({
+      title: "Grinta",
+      logo: {
+        src: "./public/logo.svg",
+        replacesTitle: true,
+      },
+      social: {
+        github: "https://github.com/getgrinta/grinta",
+        "x.com": "https://x.com/getgrinta",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            { label: "Getting Started", slug: "guides" },
+            {
+              label: "Replace system Spotlight",
+              slug: "guides/replace-spotlight",
+            },
+            { label: "Core Concepts", slug: "guides/core-concepts" },
+          ],
+        },
+        {
+          label: "Core Features",
+          items: [
+            { label: "Web and AI Search", slug: "features/search" },
+            { label: "Notes", slug: "features/notes" },
+            { label: "File System Search", slug: "features/file-system" },
+            { label: "Calculator", slug: "features/calculator" },
+            { label: "Natural Language Processing", slug: "features/nlp" },
+            { label: "Shortcuts", slug: "features/shortcuts" },
+            { label: "Clipboard History", slug: "features/clipboard" },
+            { label: "Widgets", slug: "features/widgets" },
+          ],
+        },
+        {
+          label: "Grinta Pro",
+          items: [{ label: "Notes AI", slug: "pro/notes" }],
+        },
+        {
+          label: "Recipes",
+          items: [
+            { label: "Notes in Obsidian", slug: "recipes/obsidian" },
+            { label: "Running Shell Commands", slug: "recipes/shell-commands" },
+            { label: "Speech to Text", slug: "recipes/speech-to-text" },
+          ],
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+      head: [
+        {
+          tag: "script",
+          content: `
 						window.op = window.op||function(...args){(window.op.q=window.op.q||[]).push(args);};
 						window.op('init', {
 							clientId: '11152ed2-ba79-480c-a020-028c574cb89f',
@@ -71,30 +72,30 @@ export default defineConfig({
 							trackOutgoingLinks: true,
 							trackAttributes: true,
 						});
-					`
-				},
-				{
-					tag: "script",
-					attrs: {
-						defer: true,
-						async: true,
-						src: "https://openpanel.dev/op1.js",
-					},
-				},
-				{
-					tag: "script",
-					attrs: {
-						defer: true,
-						src: "https://unpkg.com/tailwindcss-intersect@2.x.x/dist/observer.min.js",
-					},
-				},
-			],
-			components: {
-				ThemeProvider: "./src/components/theme.astro",
-				ThemeSelect: "./src/components/theme-select.astro",
-			},
-			plugins: [starlightBlog({ title: "Changelog" })],
-		}),
-		svelte(),
-	],
+					`,
+        },
+        {
+          tag: "script",
+          attrs: {
+            defer: true,
+            src: "https://umami.getgrinta.com/script.js",
+            "data-website-id": "4cd27316-f769-454e-8ec1-46ddd469d336",
+          },
+        },
+        {
+          tag: "script",
+          attrs: {
+            defer: true,
+            src: "https://unpkg.com/tailwindcss-intersect@2.x.x/dist/observer.min.js",
+          },
+        },
+      ],
+      components: {
+        ThemeProvider: "./src/components/theme.astro",
+        ThemeSelect: "./src/components/theme-select.astro",
+      },
+      plugins: [starlightBlog({ title: "Changelog" })],
+    }),
+    svelte(),
+  ],
 });
