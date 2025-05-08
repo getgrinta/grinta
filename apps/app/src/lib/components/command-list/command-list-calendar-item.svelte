08082c4b1e36c16b1d034eebc023dd488825cecf
Aleksander Lorenc <thaxsillion@gmail.com>
2025-05-08 10:02:31 +0200

    fix(calendar): Max calendar item width

--- apps/app/src/lib/components/command-list/command-list-calendar-item.svelte
+++ apps/app/src/lib/components/command-list/command-list-calendar-item.svelte
@@ -113,7 +113,7 @@
       class="flex flex-1 h-full gap-4 py-[0.5rem] items-center cursor-pointer"
     >
       <CommandListIcon label={currentLabel} {item} {active} />
-      <div class="flex flex-col align-left flex-1 min-w-0">
+      <div class="flex flex-col align-left flex-1 min-w-0 max-w-[600px]">
         <h2 class="flex-1 flex text-left font-semibold truncate max-w-full">
           {#each highlightedText as chunk}
             {@const last =
